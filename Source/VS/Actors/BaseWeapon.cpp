72d600ec6f9cc3a073021ac5dd01ab217fead0ea
Spranky <fvs@zone.ru>
2024-07-26 22:16:23 +0300

    Hot Fix blend spaces #2

--- Source/VS/Actors/BaseWeapon.cpp
+++ Source/VS/Actors/BaseWeapon.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Net/UnrealNetwork.h"
 #include "BaseWeapon.h"
 
 // Sets default values
@@ -41,14 +41,69 @@ void ABaseWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (HasAuthority())
+	{
+		FireTick(DeltaTime);
+		ReloadTick(DeltaTime);
+		DispersionTick(DeltaTime);
+		//ClipDropTick(DeltaTime);
+		//ShellDropTick(DeltaTime);
+	}
+
+}
+
+void ABaseWeapon::ReloadTick(float DeltaTime)
+{
+	if (WeaponReloading)
+	{
+		if (ReloadTimer < 0.0f)
+		{
+			FinishReload();
+		}
+		else
+		{
+			ReloadTimer -= DeltaTime;
+		}
+	}
 }
 
 void ABaseWeapon::FireTick(float DeltaTime)
 {
+	if (WeaponFiring && GetWeaponRound() > 0 && !WeaponReloading)
+	{
+		if (FireTime < 0.f)
+		{
+			Fire();
+		}
+		else
+			FireTime -= DeltaTime;
+	}
 }
 
 void ABaseWeapon::DispersionTick(float DeltaTime)
 {
+	if (!WeaponReloading) 
+	{
+		if (!WeaponFiring)
+		{
+			if (ShouldReduseDispersion) 
+				CurrentDispersion = CurrentDispersion - CurrentDispersionReduction;
+			else                        
+				CurrentDispersion = CurrentDispersion + CurrentDispersionReduction;
+		}
+
+		if (CurrentDispersion < CurrentDispersionMin)
+		{
+			CurrentDispersion = CurrentDispersionMin;
+		}
+		else
+		{
+			if (CurrentDispersion > CurrentDispersionMax)
+			{
+				CurrentDispersion = CurrentDispersionMax;
+			}
+		}
+	}
 }
 
 void ABaseWeapon::WeaponInit()
@@ -64,3 +119,192 @@ void ABaseWeapon::WeaponInit()
 	}
 }
 
+void ABaseWeapon::Fire()
+{
+	//UAnimMontage* AnimToPlay = nullptr;
+
+	///*if (WeaponAiming)
+	//	AnimToPlay = WeaponSetting.AnimWeaponInfo.AnimCharFireAim;
+	//else
+	//	AnimToPlay = WeaponSetting.AnimWeaponInfo.AnimCharFire;*/
+
+	//if (WeaponSetting.AnimWeaponInfo.AnimWeaponFire)
+	//{
+	//	AnimWeaponStart_Multicast(WeaponSetting.AnimWeaponInfo.AnimWeaponFire);
+	//}
+
+
+	///*if (WeaponSetting.ShellBullets.DropMesh)
+	//{
+	//	if (WeaponSetting.ShellBullets.DropMeshTime < 0.0f)
+	//	{
+	//		InitDropMesh_OnServer(WeaponSetting.ShellBullets.DropMesh,
+	//			WeaponSetting.ShellBullets.DropMeshOffset,
+	//			WeaponSetting.ShellBullets.DropMeshImpulseDir,
+	//			WeaponSetting.ShellBullets.DropMeshLifeTime,
+	//			WeaponSetting.ShellBullets.ImpulseRandomDispersion,
+	//			WeaponSetting.ShellBullets.PowerImpulse,
+	//			WeaponSetting.ShellBullets.CustomMass);
+	//	}
+	//	else
+	//	{
+	//		DropShellFlag = true;
+	//		DropShellTimer = WeaponSetting.ShellBullets.DropMeshTime;
+	//	}
+	//}
+	//
+	//FireTime = WeaponSetting.RateOfFire;
+
+	//AdditionalWeaponInfo.Round = AdditionalWeaponInfo.Round - 1; \
+	//	ChangeDispersionByShoot();*/
+
+	//OnWeaponFireStart.Broadcast(AnimToPlay);
+
+	//FXWeaponFire_Multicast(WeaponSetting.EffectFireWeapon, WeaponSetting.SoundFireWeapon);
+
+	//int8 NumberProjectile = GetNumberProjectileByShot();
+
+	//if (ShootLocation)
+	//{
+	//																
+	//	FVector SpawnLocation = ShootLocation->GetComponentLocation();
+	//	FRotator SpawnRotation = ShootLocation->GetComponentRotation();
+	//	
+	//	FProjectileInfo ProjectileInfo;
+	//	ProjectileInfo = GetProjectile();
+	//	FVector EndLocation;
+
+	//
+
+	//	for (int8 i = 0; i < NumberProjectile; i++)
+	//	{
+	//		EndLocation = GetFireEndLocation();
+
+	//	
+	//		if (ProjectileInfo.Projectile)
+	//		{
+	//			
+	//			FVector Dir = /*ShootEndLocation*/ GetFireEndLocation() - SpawnLocation;
+	//			Dir.Normalize();
+	//		
+	//			FMatrix myMatrix(Dir, FVector(0, 0, 0), FVector(0, 0, 0), FVector::ZeroVector);
+	//			SpawnRotation = myMatrix.Rotator();
+
+	//			//Projectile Init ballistic fire
+	//		
+	//			FActorSpawnParameters SpawnParams;
+	//			SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	//			SpawnParams.Owner = GetOwner();
+	//			SpawnParams.Instigator = GetInstigator();
+
+	//			AProjectileDefault* myProjectile = Cast<AProjectileDefault>(GetWorld()->SpawnActor(ProjectileInfo.Projectile, &SpawnLocation, &SpawnRotation, SpawnParams));
+	//			if (myProjectile /* && myProjectile != nullptr*/)
+	//			{
+	//				//ToDo Init Projectile settings by id in table row(or keep in weapon table)
+	//				// 
+	//				
+	//				//myProjectile->InitialLifeSpan = 20.0f;
+	//				//Projectile->BulletProjectileMovement->InitialSpeed = 2500.0f;
+
+	//				myProjectile->InitProjectile(WeaponSetting.ProjectileSetting);
+	//			}
+	//		}
+}
+
+void ABaseWeapon::InitReload()
+{
+	WeaponReloading = true;
+	ReloadTimer = 1.8f/*WeaponSetting.ReloadTime*/; 
+
+	//if (WeaponSetting.AnimCharReload)
+	//{
+	//	OnWeaponReloadStart.Broadcast(WeaponSetting.AnimCharReload);
+	//	AnimWeaponStart_Multicast(WeaponSetting.AnimCharReload);
+	//}
+
+	
+	//if (WeaponSetting.ClipDropMesh.DropMesh)
+	//{
+	//	DropClipFlag = true;
+	//	DropClipTimer = WeaponSetting.ClipDropMesh.DropMeshTime;
+	//}
+}
+
+void ABaseWeapon::FinishReload()
+{
+	WeaponReloading = false;
+
+	//int8 AviableAmmoFromInventory = GetAviableAmmoForReload();
+	//int8 AmmoNeedTakeFromInv;
+	//int8 NeedToReload = WeaponSetting.MaxRound - AdditionalWeaponInfo.Round;
+
+	//if (NeedToReload > AviableAmmoFromInventory)
+	//{
+	//	AdditionalWeaponInfo.Round = AviableAmmoFromInventory;
+	//	AmmoNeedTakeFromInv = AviableAmmoFromInventory;
+	//}
+	//else
+	//{
+	//	AdditionalWeaponInfo.Round += NeedToReload;
+	//	AmmoNeedTakeFromInv = NeedToReload;
+	//}
+
+
+	//OnWeaponReloadEnd.Broadcast(true, -AmmoNeedTakeFromInv);
+}
+
+void ABaseWeapon::CancelReload()
+{
+	WeaponReloading = false;
+	/*if (SkeletalMeshWeapon && SkeletalMeshWeapon->GetAnimInstance())
+		SkeletalMeshWeapon->GetAnimInstance()->StopAllMontages(0.15f);
+
+	OnWeaponReloadEnd.Broadcast(false, 0);*/
+}
+
+bool ABaseWeapon::CheckWeaponCanFire()
+{
+	return !BlockFire;
+}
+
+bool ABaseWeapon::CheckCanWeaponReload()
+{
+	/*bool result = true;
+	if (GetOwner())
+	{
+		UTPSInventoryComponent* MyInv = Cast<UTPSInventoryComponent>(GetOwner()->GetComponentByClass(UTPSInventoryComponent::StaticClass()));
+		if (MyInv)
+		{
+			int8 AviableAmmoForWeapon;
+			if (!MyInv->CheckAmmoForWeapon(WeaponSetting.WeaponType, AviableAmmoForWeapon))
+			{
+				result = false;
+			}
+		}
+	}
+	return result;*/
+	return true;
+}
+
+int32 ABaseWeapon::GetWeaponRound()
+{
+	return int32();
+}
+
+void ABaseWeapon::SetWeaponStateFire_OnServer_Implementation(bool bIsFire)
+{
+	if (CheckWeaponCanFire())
+		WeaponFiring = bIsFire;
+	else
+		WeaponFiring = false;
+	FireTime = 0.01f;
+}
+
+void ABaseWeapon::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//DOREPLIFETIME(ABaseWeapon, AdditionalWeaponInfo);
+	DOREPLIFETIME(ABaseWeapon, WeaponReloading);
+	//DOREPLIFETIME(AWeaponDefault, ShootEndLocation);
+}
--- Source/VS/Actors/BaseWeapon.h
+++ Source/VS/Actors/BaseWeapon.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -28,6 +28,39 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAcess = "true"), Category = Components)
 	class UArrowComponent* ShootLocation = nullptr;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon logic")
+	bool WeaponFiring = false;
+
+	UPROPERTY(Replicated, EditAnywhere, BlueprintReadWrite, Category = "Weapon logic")
+	bool WeaponReloading = false;
+
+	float FireTime = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ReloadLogic")
+		
+	bool WeaponAiming = false;
+
+	bool BlockFire = false;
+
+	bool DropClipFlag = false;
+
+	bool DropShellFlag = false;
+
+	float DropClipTimer = -1.0f;
+
+	float DropShellTimer = -1.0f;
+
+	float ReloadTimer = 0.0f;
+
+	// Ðàçáðîñ
+	//UPROPERTY(Replicated)
+	bool ShouldReduseDispersion = false;
+	float CurrentDispersion = 0.0f;
+	float CurrentDispersionMax = 1.0f;
+	float CurrentDispersionMin = 0.1f;
+	float CurrentDispersionRecoil = 0.1f;
+	float CurrentDispersionReduction = 0.1f;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -36,10 +69,31 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void ReloadTick(float DeltaTime);
+
 	void FireTick(float DeltaTime);
 
 	void DispersionTick(float DeltaTime);
 
 	void WeaponInit();
 
+	void Fire();
+
+	void InitReload();
+
+	void FinishReload();
+
+	void CancelReload();
+
+	bool CheckWeaponCanFire();
+
+	bool CheckCanWeaponReload();
+
+	UFUNCTION(BlueprintCallable)
+	int32 GetWeaponRound();
+
+	UFUNCTION(Server, Reliable, BlueprintCallable)
+	void SetWeaponStateFire_OnServer(bool bIsFire);
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
 };
